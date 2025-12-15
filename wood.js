2350cbdee0e7189339e926fc29e678959e270151
RGAntu <rgantu887@gmail.com>
2025-12-15 12:28:58 +0600

    added woodQuantity in wood.js

---
+++ wood.js
@@ -0,0 +1,23 @@
+/**
+ * chair --> 3 cft 
+ * table --> 10 cft
+ * bed --> 50 cft
+ */
+
+function woodQuantity( chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+    
+    const totalChairWood = perChairWood * chairQuantity;
+    const totalTableWood = perTableWood * tableQuantity;
+    const totalBedWood = perBedWood * bedQuantity;
+
+    const totalWood = totalChairWood + totalTableWood + totalBedWood;
+
+    return totalWood;
+
+}
+
+const wood = woodQuantity(5, 0, 1);
+console.log("Wood needed",wood);
